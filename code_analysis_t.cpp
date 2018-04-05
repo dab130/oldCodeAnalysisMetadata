a59865e5d93c67b3840942476e2e1180ec87063c
root <root@BestTop.summitoh.net>
2018-04-05 10:03:57 -0400

    added another test case for given_filename. Test uses a (dash) and uses (data) for entry_filename

--- code_analysis_t.cpp
+++ code_analysis_t.cpp
@@ -139,6 +139,48 @@ int main() {
         assert(code_analysis(request) == false);
 		
 	}
+	{
+		//Test case: given_filename with a dash
+		//The given_filename is a single dash “-” when the input is from standard input. In this case, the option_filename 
+		//and entry_filename must be used. Note that the entry_filename is not valid (i.e, “data”) for a non source code archive,
+		//and the option_filename must be used. If the option_filename is blank (i.e., “”), then the resulting filename is blank,
+		//and the language is based on the option_language.
+        analysis_request request;
+        request.given_filename  = "-";
+        request.entry_filename  = "";
+        request.given_url       = "";
+        request.option_filename = "testing.cpp";
+        request.option_url      = "";
+        request.option_language = "";
+		
+        auto filename = analysis_filename(request);
+        assert(filename == "testing.cpp");
+        assert(analysis_url(request) == "");
+        assert(analysis_language(request, filename) == "C++");
+        assert(code_analysis(request) == false);
+		
+	}
+	{
+		//Test case: given_filename with a dash as a non source code archive.
+		//The given_filename is a single dash “-” when the input is from standard input. In this case, the option_filename 
+		//and entry_filename must be used. Note that the entry_filename is not valid (i.e, “data”) for a non source code archive,
+		//and the option_filename must be used. If the option_filename is blank (i.e., “”), then the resulting filename is blank,
+		//and the language is based on the option_language.
+        analysis_request request;
+        request.given_filename  = "-";
+        request.entry_filename  = "data";
+        request.given_url       = "";
+        request.option_filename = "testing.cpp";
+        request.option_url      = "";
+        request.option_language = "";
+		
+        auto filename = analysis_filename(request);
+        assert(filename == "testing.cpp");
+        assert(analysis_url(request) == "");
+        assert(analysis_language(request, filename) == "C++");
+        assert(code_analysis(request) == false);
+		
+	}
 
 	
     return 0;
